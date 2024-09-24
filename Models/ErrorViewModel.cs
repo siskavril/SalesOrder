81a4ef88ecb7de0101e54afb5781fff96f891f6a
firda <firda@DAUS-PC>
2024-09-24 18:10:14 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesOrder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
